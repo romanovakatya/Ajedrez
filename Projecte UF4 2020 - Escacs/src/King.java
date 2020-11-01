ca1f61f9e69e62f71033f0236f681030907ded74
romanovakatya <katerina.bogrom@gmail.com>
2020-11-01 11:59:17 +0100

    prueba del primer commit de branch katya

--- Projecte UF4 2020 - Escacs/src/King.java
+++ Projecte UF4 2020 - Escacs/src/King.java
@@ -1,10 +1,10 @@
 //Rey K k
 public class King extends Piece{
-
+	public String king;
 	//constructor,
 	public King(char positionColumn, int positionRow, String colour) {
 		super(positionColumn, positionRow, colour);
-		
+		this.king = "prueba";
 	}
 	
 	//métodos,
