916299c1228a9a1a958fe4c2e8ecc48e3ed257d5
romanovakatya <katerina.bogrom@gmail.com>
2020-11-01 12:06:01 +0100

    prueba de branch khadija

--- Projecte UF4 2020 - Escacs/src/King.java
+++ Projecte UF4 2020 - Escacs/src/King.java
@@ -1,10 +1,10 @@
 //Rey K k
 public class King extends Piece{
-
+	public String queen;
 	//constructor,
 	public King(char positionColumn, int positionRow, String colour) {
 		super(positionColumn, positionRow, colour);
-		
+		this.queen = "tonta";
 	}
 	
 	//métodos,
